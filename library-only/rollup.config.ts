cdc5a39c3453043cf7683703e6aacaeeaae3c148
4Nameless4 <623507166@qq.com>
2023-12-19 23:01:43 +0800

    some rollup config bug

--- library-only/rollup.config.ts
+++ library-only/rollup.config.ts
@@ -3,21 +3,19 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
+import type { RollupOptions } from "rollup";
 
-const output = {
-  dir: "lib",
-  entryFileNames: "[name]-[hash].js",
-};
-
-const rollup = {
+const rollup: RollupOptions = {
   input: "src/index.ts",
   output: [
     {
-      ...output,
+      dir: "lib",
+      entryFileNames: "index.mjs",
       format: "esm",
     },
     {
-      ...output,
+      dir: "lib",
+      entryFileNames: "index.cjs",
       format: "cjs",
     },
   ],
